2b227051d1f11eb27e49686447c5eaa9a77149d5
Pinelli <ricardosilvapinelli@gmail.com>
2023-02-25 23:40:45 -0300

    + closures

--- Fundamentos/closures.js
+++ Fundamentos/closures.js
@@ -1,3 +1,13 @@
+/*
+    As closures mantêm o estado e o comportamento do escopo 
+  externo, armazenando referências às variáveis e funções 
+  necessárias para sua execução, mesmo quando o escopo 
+  externo é destruído. 
+    Isso permite que elas criem ambientes privados e 
+  encapsulados e realizem operações complexas com base 
+  em dados e comportamentos externos.
+*/
+
 
 function amount(){
   let amount = 0
