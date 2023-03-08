3ecd1d6eeecff9c0b562ab3b8223ae34112aea85
Pinelli <ricardosilvapinelli@gmail.com>
2023-03-08 19:29:15 -0300

    .bind()

--- others/this.js
+++ others/this.js
@@ -1,8 +1,18 @@
 const person = {
   name: 'Ricardo',
-  goingDo: function() {
+  goingDo() {
     return `Eu sou ${this.name}`
   }
 }
 
 console.log(person.goingDo()) // Eu sou Ricardo
+
+const personDo = person.goingDo
+console.log(personDo) // Eu sou Ricardo
+
+
+// Para nao dar conflito de paradigmas (funcional, OOP), usar metodo .bind()
+
+const personDo_2 = person.goingDo.bind(person)
+
+console.log(personDo_2()) // Eu sou Ricardo
